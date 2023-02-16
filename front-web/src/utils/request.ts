3a1f6b6e31c43037842cf7af96f99218fec7a05c
sergiocarvalho-48 <sergio_m-carvalho@hotmail.com>
2023-02-16 17:03:58 -0300

    Integrate FilterData, FilterVendas, FilterPizza, FilterSummary

--- front-web/src/utils/request.ts
+++ front-web/src/utils/request.ts
@@ -1,7 +1,17 @@
+import { FilterData } from './../types';
 import axios from 'axios';
+import { formatDateToServer } from './fortmatters';
 
 const baseURL = 'http://localhost:8080';
 
 export const makeRequest = axios.create({
   baseURL
 });
+
+export const buildFilterParams = (filterData?: FilterData) => {
+  return {
+    minDate: formatDateToServer(filterData?.dates?.[0]),
+    maxDate: formatDateToServer(filterData?.dates?.[1]),
+    gender: filterData?.gender
+  };
+};
